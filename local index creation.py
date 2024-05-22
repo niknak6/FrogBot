5e2dd9fb6219236201c1d4386575bd47cde01bbd
idnot <thecrazyasylum@gmail.com>
2024-05-22 12:05:49 -0400

    Update local index creation.py

--- local index creation.py
+++ local index creation.py
@@ -16,28 +16,19 @@ load_dotenv()
 github_client = GithubClient(os.getenv('GITHUB_TOKEN'))
 Settings.embed_model = HuggingFaceEmbedding(model_name="BAAI/bge-small-en-v1.5")
 chroma_db = chromadb.PersistentClient(path="./chroma_db")
-all_docs = {}
 
 '''DISCORD DATA'''
-print("Documents loaded successfully. Loading local files...")
+print("Loading local files...")
 dir_path = 'DiscordDocs'
 reader = SimpleDirectoryReader(input_dir=dir_path, required_exts=[".txt"])
-local_docs = reader.load_data()
-all_docs.update({"local_files": local_docs})
-print("Local files loaded successfully.")
+discord_docs = reader.load_data()
 
-print("Discord data downloaded successfully. Setting up vector store...")
-discord_collection = chroma_db.create_collection("discord-data")
+print("Local files loaded successfully. Setting up vector store for Discord data...")
+discord_collection = chroma_db.get_or_create_collection("discord-data")
 discord_vector_store = ChromaVectorStore(chroma_collection=discord_collection)
 discord_storage_context = StorageContext.from_defaults(vector_store=discord_vector_store)
-print("Discord data vector store setup complete.")
 
-documents = []
-for repo_docs in tqdm(all_docs.values(), desc="Flattening documents"):
-    documents.extend(repo_docs)
-print("Discord data documents flattened successfully. Setting up Discord data index...")
-
-discord_index = VectorStoreIndex.from_documents(documents, storage_context=discord_storage_context, show_progress=True)
+discord_index = VectorStoreIndex.from_documents(discord_docs, storage_context=discord_storage_context, show_progress=True)
 print("Discord data setup complete.")
 
 '''WIKI DATA'''
@@ -53,13 +44,56 @@ wiki_collection = chroma_db.get_or_create_collection("wiki-data")
 wiki_vector_store = ChromaVectorStore(chroma_collection=wiki_collection)
 wiki_storage_context = StorageContext.from_defaults(vector_store=wiki_vector_store)
 
-wiki_documents = []
-for wiki_doc in tqdm(wiki, desc="Flattening wiki documents"):
-    wiki_documents.append(wiki_doc)
-print("Wiki documents flattened successfully. Setting up index for wiki...")
-
-wiki_index = VectorStoreIndex.from_documents(wiki_documents, storage_context=wiki_storage_context, show_progress=True)
+wiki_index = VectorStoreIndex.from_documents(wiki, storage_context=wiki_storage_context, show_progress=True)
 print("Wiki index setup complete.")
+
+'''RETROPILOT STOREFRONT DATA'''
+retropilot_scraper = WholeSiteReader(
+    prefix="https://shop.retropilot.org/", max_depth=2
+)
+retropilot_data = retropilot_scraper.load_data(
+    base_url="https://shop.retropilot.org/"
+)
+
+print("RetroPilot data downloaded successfully. Setting up vector store for RetroPilot...")
+retropilot_collection = chroma_db.get_or_create_collection("retropilot-data")
+retropilot_vector_store = ChromaVectorStore(chroma_collection=retropilot_collection)
+retropilot_storage_context = StorageContext.from_defaults(vector_store=retropilot_vector_store)
+
+retropilot_index = VectorStoreIndex.from_documents(retropilot_data, storage_context=retropilot_storage_context, show_progress=True)
+print("RetroPilot index setup complete.")
+
+'''ONECLONE DATA'''
+oneclone_scraper = WholeSiteReader(
+    prefix="https://oneclone.net/", max_depth=2
+)
+oneclone_data = oneclone_scraper.load_data(
+    base_url="https://oneclone.net/"
+)
+
+print("OneClone data downloaded successfully. Setting up vector store for OneClone...")
+oneclone_collection = chroma_db.get_or_create_collection("oneclone-data")
+oneclone_vector_store = ChromaVectorStore(chroma_collection=oneclone_collection)
+oneclone_storage_context = StorageContext.from_defaults(vector_store=oneclone_vector_store)
+
+oneclone_index = VectorStoreIndex.from_documents(oneclone_data, storage_context=oneclone_storage_context, show_progress=True)
+print("OneClone index setup complete.")
+
+'''SPRINGER ELECTRONICS DATA'''
+springer_scraper = WholeSiteReader(
+    prefix="https://springerelectronics.com/", max_depth=2
+)
+springer_data = springer_scraper.load_data(
+    base_url="https://springerelectronics.com/"
+)
+
+print("Springer Electronics data downloaded successfully. Setting up vector store for Springer Electronics...")
+springer_collection = chroma_db.get_or_create_collection("springer-electronics-data")
+springer_vector_store = ChromaVectorStore(chroma_collection=springer_collection)
+springer_storage_context = StorageContext.from_defaults(vector_store=springer_vector_store)
+
+springer_index = VectorStoreIndex.from_documents(springer_data, storage_context=springer_storage_context, show_progress=True)
+print("Springer Electronics index setup complete.")
 
 '''GITHUB DATA'''
 repos_config = [
@@ -107,19 +141,13 @@ for config in tqdm(repos_config, desc="Loading documents from repositories"):
             retries=3,
         )
         documents = loader.load_data(branch=config["branch"])
-        all_docs.update({f"{config['owner']}/{config['repo']}": documents})
 
         print(f"{config['owner']}/{config['repo']} data downloaded successfully. Setting up vector store...")
         github_collection = chroma_db.create_collection(f"{config['owner']}-{config['repo']}-data")
         github_vector_store = ChromaVectorStore(chroma_collection=github_collection)
         github_storage_context = StorageContext.from_defaults(vector_store=github_vector_store)
 
-        github_documents = []
-        for repo_docs in tqdm(all_docs.values(), desc="Flattening documents"):
-            github_documents.extend(repo_docs)
-        print(f"{config['owner']}/{config['repo']} documents flattened successfully. Setting up index...")
-
-        github_index = VectorStoreIndex.from_documents(github_documents, storage_context=github_storage_context, show_progress=True)
+        github_index = VectorStoreIndex.from_documents(documents, storage_context=github_storage_context, show_progress=True)
         print(f"{config['owner']}/{config['repo']} index setup complete.")
 
     except httpx.ConnectTimeout:
